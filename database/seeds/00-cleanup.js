0fe38dfd1637626d578c95bf7357fbd9953f514a
patrick-replogle <patrickr1138@gmail.com>
2020-03-04 15:08:36 -0800

    tweaked seed files

--- database/seeds/00-cleanup.js
+++ database/seeds/00-cleanup.js
@@ -1,5 +1,10 @@
 const cleaner = require("knex-cleaner");
 
 exports.seed = function(knex) {
-  return cleaner.clean(knex);
+  return cleaner.clean(knex, {
+    // resets ids
+    mode: "delete",
+    // don't empty migration tables
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+  });
 };
