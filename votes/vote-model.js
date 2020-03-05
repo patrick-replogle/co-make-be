2344011aa31753df7628586f1ae4fc77af6a9d6e
patrick-replogle <patrickr1138@gmail.com>
2020-03-04 20:58:04 -0800

    adjusted schema so that user can only vote once

---
+++ votes/vote-model.js
@@ -0,0 +1,25 @@
+const db = require("../database/dbConfig.js");
+
+module.exports = {
+  findVotesForPost,
+  findVoteById,
+  addVote
+};
+
+function findVotesForPost(userId, postId) {
+  return db("votes")
+    .where("user_id", userId)
+    .andWhere("post_id", postId);
+}
+
+function findVoteById(id) {
+  return db("votes")
+    .where({ id })
+    .first();
+}
+
+async function addVote(vote) {
+  const [id] = await db("votes").insert(vote, "id");
+
+  return findVoteById(id);
+}
