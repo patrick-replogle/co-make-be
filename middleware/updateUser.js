43978c5dffb5e47fdac54c96fdcf96a17f2a8067
patrick-replogle <patrickr1138@gmail.com>
2020-03-16 09:59:13 -0700

    updated middleware to update user

---
+++ middleware/updateUser.js
@@ -0,0 +1,19 @@
+function updateUser(req, res, next) {
+  if (
+    !req.body.username ||
+    !req.body.first_name ||
+    !req.body.last_name ||
+    !req.body.email
+  ) {
+    res.status(400).json({
+      message:
+        "username, first_name, last_name, and email are all required fields"
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  updateUser
+};
