e227a0df0aa71d32cbdc33094f04882725495433
patrick-replogle <patrickr1138@gmail.com>
2020-06-23 08:58:15 -0700

    reconfigured knex file and updated migration

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,49 @@
-const dbConnection = process.env.DATABASE_URL;
+// const dbConnection = process.env.DATABASE_URL;
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: "pg",
     connection: {
-      filename: "./database/co-make.db3"
-    },
-    useNullAsDefault: true,
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
+      host: "localhost",
+      database: process.env.DEV_DATABASE,
     },
     migrations: {
-      directory: "./database/migrations"
+      directory: "./database/migrations",
+      tablename: "knex_migrations",
     },
     seeds: {
-      directory: "./database/seeds"
-    }
+      directory: "./database/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    useNullAsDefault: true,
   },
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./database/test.db3"
+      filename: "./database/test.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./database/migrations"
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./database/seeds"
-    }
+      directory: "./database/seeds",
+    },
   },
 
   production: {
     client: "pg",
-    connection: dbConnection,
+    connection: process.env.DEV_DATABASE,
     useNullAsDefault: true,
     migrations: {
-      directory: "./database/migrations"
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./database/seeds"
-    }
-  }
+      directory: "./database/seeds",
+    },
+  },
 };
--- users/user-router.js
+++ users/user-router.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs");
 
 const Users = require("./user-model.js");
 const { updateUser } = require("../middleware/updateUser.js");
